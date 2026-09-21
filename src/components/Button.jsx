a18023108d303063fcbff2d8842962dfe616f6be
Hayden Swain-Schmit <swainschmity@gmail.com>
2026-09-21 15:03:20 -0600

    styled components (external library)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,29 @@
-import './Button.css';
+// import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
+
+import styled from 'styled-components';
+
+const Button = styled.button`
+
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: none;
+  background-color: #492365;
+  color: #ffffff;
+  cursor: pointer;
+
+
+  &:hover,
+  &:active {
+  background-color: #36194b;
+}
+`;
 
 export default Button;
